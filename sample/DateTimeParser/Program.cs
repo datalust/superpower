4c9894818df1474311f03ed67e980a6b402dc701
Lukas Platzer <lukas.platzer@delfortgroup.com>
2017-06-28 17:47:45 +0200

    inital commit for datetime parser example

---
+++ sample/DateTimeParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateTimeParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
