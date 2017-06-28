0d6467b597e6edb44c29791aac406db4f391e4be
Lukas Platzer <lukas.platzer@delfortgroup.com>
2017-06-28 17:50:49 +0200

    add example call for datetimeparser

--- sample/DateTimeParser/Program.cs
+++ sample/DateTimeParser/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using Superpower;
 
 namespace DateTimeParser
 {
     class Program
     {
+        static void ParseAndPrint(string input)
+        {
+            try
+            {
+                var dt = DateTimeParser.DateTime.Parse(input);
+                Console.WriteLine("Input: '{0}', ParsedValue: '{1}'", input, dt.ToString("o"));
+            }
+            catch (System.Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ParseAndPrint("12:38");
+            ParseAndPrint("12:38:10");
+            ParseAndPrint("2017-01-01");
+            ParseAndPrint("2017-01-01 05:28:10");
+            ParseAndPrint("2017-01-01 05:28");
+            ParseAndPrint("\"2017-01-01\"");
+            ParseAndPrint("\"2017-01-01");
+            ParseAndPrint("2017-01-01 05:x8:10");
+            ParseAndPrint("2017-01-01T05:28:10");
+            ParseAndPrint("2017-01-01T05:28");
         }
     }
 }
