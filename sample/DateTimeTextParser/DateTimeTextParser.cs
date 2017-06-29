cffe7d11d76cb21d60451d3e0c9a6d117f93e67a
Lukas Platzer <lukas.platzer@delfortgroup.com>
2017-06-29 20:26:16 +0200

    removed unnecessary lambda

--- sample/DateTimeTextParser/DateTimeTextParser.cs
+++ sample/DateTimeTextParser/DateTimeTextParser.cs
@@ -20,27 +20,27 @@ namespace DateTimeTextParser
 
         static TextParser<string> MonthOfDate = 
             TwoDigits;
-            
+
         static TextParser<string> DayOfDate = 
             TwoDigits;
 
         public static TextParser<DateTime> Date = 
-            from year in YearOfDate.Select(_ => Int32.Parse(_))
+            from year in YearOfDate.Select(Int32.Parse)
             from sep1 in Character.EqualTo('-')
-            from mon in MonthOfDate.Select(_ => Int32.Parse(_))
+            from mon in MonthOfDate.Select(Int32.Parse)
             from sep2 in Character.EqualTo('-')
-            from day in DayOfDate.Select(_ => Int32.Parse(_))
+            from day in DayOfDate.Select(Int32.Parse)
             select new DateTime(year, mon, day);
 
         static TextParser<int> secondWithSep = 
             from sep in Character.EqualTo(':')
-            from second in TwoDigits.Select(_ => Int32.Parse(_))
+            from second in TwoDigits.Select(Int32.Parse)
             select second;
 
         public static TextParser<TimeSpan> Time =
-            from hour in TwoDigits.Select(_ => Int32.Parse(_))
+            from hour in TwoDigits.Select(Int32.Parse)
             from sep1 in Character.EqualTo(':')
-            from minute in TwoDigits.Select(_ => Int32.Parse(_))
+            from minute in TwoDigits.Select(Int32.Parse)
             from second in secondWithSep.OptionalOrDefault()
             select new TimeSpan(hour, minute, second);
 
