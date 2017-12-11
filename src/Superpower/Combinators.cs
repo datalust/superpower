f4dd940d30dd9413058fe060197545f90bb03fe1
Anders Rasmussen <asr@configit.com>
2017-12-11 14:57:29 +0100

    Added Between() combinator

--- src/Superpower/Combinators.cs
+++ src/Superpower/Combinators.cs
@@ -177,6 +177,39 @@ namespace Superpower
         }
 
         /// <summary>
+        /// Construct a parser that matches <paramref name="left"/>, discards the resulting value,
+        /// then matches <paramref name="parser"/>, keeps the value, then matches <paramref name="right"/>
+        /// and returns the value matched by <paramref name="parser"/>.
+        /// </summary>
+        /// <typeparam name="TKind">The kind of the tokens being parsed.</typeparam>
+        /// <typeparam name="T">The type of value being parsed.</typeparam>
+        /// <typeparam name="U">The type of the resulting value.</typeparam>
+        /// <param name="parser">The parser.</param>
+        /// <param name="left">First parser to match, value is ignored.</param>
+        /// <param name="right">Last parser to match, value is ignored.</param>
+        /// <returns>The resulting parser.</returns>
+        public static TokenListParser<TKind, T> Between<TKind, T, U>(this TokenListParser<TKind, T> parser, TokenListParser<TKind, U> left, TokenListParser<TKind, U> right)
+        {
+            return left.IgnoreThen(parser.Then(right.Value));
+        }
+
+        /// <summary>
+        /// Construct a parser that matches <paramref name="left"/>, discards the resulting value,
+        /// then matches <paramref name="parser"/>, keeps the value, then matches <paramref name="right"/>
+        /// and returns the value matched by <paramref name="parser"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of value being parsed.</typeparam>
+        /// <typeparam name="U">The type of the resulting value.</typeparam>
+        /// <param name="parser">The parser.</param>
+        /// <param name="left">First parser to match, value is ignored.</param>
+        /// <param name="right">Last parser to match, value is ignored.</param>
+        /// <returns>The resulting parser.</returns>
+        public static TextParser<T> Between<T, U>(this TextParser<T> parser, TextParser<U> left, TextParser<U> right)
+        {
+            return left.IgnoreThen(parser.Then(right.Value));
+        }
+
+        /// <summary>
         /// Construct a parser that matches <paramref name="first"/>, discards the resulting value, then returns the result of <paramref name="second"/>.
         /// </summary>
         /// <typeparam name="TKind">The kind of the tokens being parsed.</typeparam>
