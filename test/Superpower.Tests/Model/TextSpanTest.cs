8213842a5a26d2d78d1d5ee74add20dae7835be2
unknown <nakase.d1220@gmail.com>
2025-09-04 22:03:27 +0900

    adds unit test for TextSpan.AsReadOnlySpan

---
+++ test/Superpower.Tests/Model/TextSpanTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Superpower.Model;
+using Xunit;
+
+namespace Superpower.Tests.Model
+{
+    public class TextSpanTest
+    {
+        [Theory]
+        [InlineData("hello", 0, 5, "hello")]
+        [InlineData("hello", 1, 3, "ell")]
+        [InlineData("hello", 2, 0, "")]
+        [InlineData("The quick brown fox jumps over the lazy dog", 9, 7, " brown ")]
+        public void AsReadOnlySpanWorks(string input, int start, int length, string expected)
+        {
+            var span = new TextSpan(input).Skip(start).First(length);
+            var readOnlySpan = span.AsReadOnlySpan();
+            Assert.Equal(expected, readOnlySpan.ToString());
+        }
+
+        [Fact]
+        public void AsReadOnlySpanEnsureHasValue()
+        {
+            Assert.Throws<InvalidOperationException>(() => TextSpan.None.AsReadOnlySpan());
+        }
+    }
+}
